fce00e94f948fbb1c14f92199106e5b8b5d2d7e0
SaasHQ <saashqdev@gmail.com>
2024-08-13 10:52:24 -0400

    update variable names

--- src/kubeworkz/elComponent/qz-editor/index.js
+++ src/kubeworkz/elComponent/qz-editor/index.js
@@ -1,7 +1,3 @@
-import { setLocaleData } from 'monaco-editor-nls';
-import en_US from 'monaco-editor-nls/locale/en_US';
-setLocaleData(en_US);
-
 import QzEditor from './QzEditor';
 
 export default QzEditor;
